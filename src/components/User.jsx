d114b259fa25b09d5f7dc86a86a8e76051f5f416
kaiweifan11 <30461398+kaiweifan11@users.noreply.github.com>
2024-10-10 21:06:45 +0800

    Add files via upload

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -18,11 +18,9 @@ const User = (props) =>{
     return (
         <Flex align={'top'} style={{height: "60px"}}>
             <Avatar size="large" src={user?.picture?.large} style={{width: "60px", height: "60px"}}/>
-            {screenSize === "xl" || screenSize === "xxl" && (
-                <Flex align={'center'}>
+            <Flex align={'center'}>
                     <Text style={usernameStyle}>{user?.login?.username}</Text>
                 </Flex>
-            )}
         </Flex>
     )
 }
